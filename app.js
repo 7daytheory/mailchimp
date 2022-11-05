b42397b3da1b7b28b3bbc134510ef2f31a690def
Matthew Lowe <Matthewjlowe11@gmail.com>
2022-11-05 19:23:47 -0400

    Added bootstrap, setup sign-in page

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const request = require('request');
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/signup.html');
+})
+
 app.listen(3000, () => {
     console.log("App listening on port 3000");
 })
